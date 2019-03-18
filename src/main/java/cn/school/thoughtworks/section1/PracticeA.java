aaa7222c2476a7ef151a34defc010af49c2e1ddc
Ruifeng-Wu <296078293@qq.com>
2019-03-18 15:13:56 +0800

    refactor:refactor PracticeA

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,12 +1,14 @@
 package cn.school.thoughtworks.section1;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
-        return new ArrayList<>(Arrays.asList("a", "e", "f", "d"));
+        List<String> arrayList1=new ArrayList<>(collection1);
+        List<String> arrayList2=new ArrayList<>(collection2);
+        arrayList1.retainAll(arrayList2);
+        return arrayList1;
     }
 }
