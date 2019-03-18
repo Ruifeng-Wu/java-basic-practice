13fd21305960bbcf1e1687b42ad547342c8d0362
Ruifeng-Wu <296078293@qq.com>
2019-03-18 20:33:48 +0800

    refactor:refactor section2/PracticeB

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,21 +1,15 @@
 package cn.school.thoughtworks.section2;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        Map<String, Integer> sameElementsNum = new HashMap<>(10);
-        for (String element: collection1) {
-            if (element.length()>1){
-                sameElementsNum.put(element.substring(0,1), Integer.valueOf(element.substring(2,3)));
-            }else {
-                if (sameElementsNum.containsKey(element)) {
-                    sameElementsNum.put(element, sameElementsNum.get(element) + 1);
-                } else {
-                    sameElementsNum.put(element, 1);
-                }
+        Map<String, Integer> sameElementsNum = new PracticeA().countSameElements(collection1);
+        for (String str:sameElementsNum.keySet()) {
+            if (str.length()>1){
+                sameElementsNum.remove(str);
+                sameElementsNum.put(str.substring(0,1), Integer.valueOf(str.substring(2,3)));
             }
         }
         return sameElementsNum;
