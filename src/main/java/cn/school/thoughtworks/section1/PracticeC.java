35a0a499ded9824eb32064eba8162d8e7e7ec8a1
Ruifeng-Wu <296078293@qq.com>
2019-03-18 15:45:33 +0800

    refactor:refactor section1/PracticeC

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,12 +1,13 @@
 package cn.school.thoughtworks.section1;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return Arrays.asList("a", "e", "f", "d");
+        List<String> arrayList1=new ArrayList<>(collection1);
+        List<String> arrayList2=collection2.get("value");
+        arrayList1.retainAll(arrayList2);
+        return arrayList1;
     }
 }
