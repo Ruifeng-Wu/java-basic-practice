192f1544c2a37a165a76a120681b7639c269e8b9
Ruifeng-Wu <296078293@qq.com>
2019-03-18 23:03:12 +0800

    feat:implement test section2/PracticeC

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,42 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.stream.Collectors.toList;
+
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> sameElementsMap = new HashMap<>();
+        List<String> regularList = collection1.stream().filter(x -> x.length() == 1).collect(toList());
+        List<String> irregularList = collection1.stream().filter(x -> x.length() > 1).collect(toList());
+        for (int i = 0; i < irregularList.size(); i++) {
+            String element = "";
+            String elementNum = "";
+            String string = irregularList.get(i);
+            for (int j = 0; j < string.length(); j++) {
+                char temp = string.charAt(j);
+                if (temp >= 'a' && temp <= 'z') {
+                    element = String.valueOf(temp);
+                }
+                if (Character.isDigit(temp)) {
+                    elementNum += temp;
+                }
+            }
+            if (sameElementsMap.containsKey(element)) {
+                sameElementsMap.put(element, sameElementsMap.get(element) + Integer.valueOf(elementNum));
+            } else {
+                sameElementsMap.put(element, Integer.valueOf(elementNum));
+            }
+        }
+        for (String x : regularList) {
+            if (sameElementsMap.containsKey(x)) {
+                sameElementsMap.put(x, sameElementsMap.get(x) + 1);
+            } else {
+                sameElementsMap.put(x, 1);
+            }
+        }
+        return sameElementsMap;
     }
 }
