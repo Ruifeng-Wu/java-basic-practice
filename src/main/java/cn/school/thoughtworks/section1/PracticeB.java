6390cb09056930488b1d676314dafa2a382d14c1
Ruifeng-Wu <296078293@qq.com>
2019-03-18 15:28:13 +0800

    refactor:refactor PracticeB

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,11 +1,19 @@
 package cn.school.thoughtworks.section1;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return Arrays.asList("a", "e", "f", "d");
+        List<String> collection2Element=new ArrayList<>();
+        List<String> arrayList1 = new ArrayList<>(collection1);
+        Iterator<List<String>> iterator = collection2.iterator();
+        while (iterator.hasNext()) {
+            collection2Element = iterator.next();
+            arrayList1.retainAll(collection2Element);
+        }
+        return arrayList1;
     }
 }
