862141e347e86ca044a63f07cb826b6e4c902ae9
Ruifeng-Wu <296078293@qq.com>
2019-03-18 20:55:50 +0800

    refactor:refactor section2/PracticeA with jdk8 new feature

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,16 +1,14 @@
 package cn.school.thoughtworks.section2;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeA {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        Map<String, Integer> sameElementsNum = new HashMap<>();
-        for (String str : collection1) {
-            sameElementsNum.put(str, Collections.frequency(collection1, str));
-        }
+        Map<String, Integer> sameElementsNum = collection1.stream()
+                .collect(Collectors.toMap(str -> str, str -> Collections.frequency(collection1, str), (a, b) -> b));
         return sameElementsNum;
     }
 }
